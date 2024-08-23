c98928b43a147a1ced479c00d948f51b9715f8b0
Alec Li <yunshen08@gmail.com>
2024-08-23 18:09:49 -0400

    fix

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from pymongo import MongoClient
 import os
 import uuid
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient("mongodb://localhost:27017")
 db = client.photo_gallery
@@ -34,7 +43,7 @@ async def upload_photo(file: UploadFile = File(...)):
     else:
         raise HTTPException(status_code=500, detail="File upload failed")
 
-@app.get("/images/previews/")
+@app.get("/images/previews")
 async def get_image_previews():
     print('getting image previews...')
     images = photos_collection.find({}, {"_id": 0, "file_id": 1, "filename": 1})
@@ -43,7 +52,9 @@ async def get_image_previews():
 
 @app.get("/images/{file_id}/preview")
 async def get_image_preview(file_id: str):
+    print('get image preview')
     image = photos_collection.find_one({"file_id": file_id})
+    print('found image', image)
     if not image:
         raise HTTPException(status_code=404, detail="Image not found")
 
